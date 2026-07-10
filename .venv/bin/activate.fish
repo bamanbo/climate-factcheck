84aef71f4fd743b8d7822182b1158c8586ba1df3
bamanbo <74776333+bamanbo@users.noreply.github.com>
2026-07-10 12:29:21 +0200

    f

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/bjornolavlilleaas/Documents/GitHub/climate-factcheck/.venv
+set -gx VIRTUAL_ENV /Users/bjornolavlilleaas/master_project/GitHub/climate-factcheck/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
